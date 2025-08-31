735727854d9b42bb68dad9cb6334443ced0ab955
Maria Yuran <mariayuran@yandex.by>
2025-08-31 11:35:08 +0300

    Changes in f/user-access

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 console.log('Hello git');
 console.log('Finally Maria learned github');
+
+const age = 20;
+if (age >= 18) {
+  console.log('You are adult');
+} else {
+  console.log('You are child');
+}
